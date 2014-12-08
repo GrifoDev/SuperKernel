c2ae3ff693b02e8ab414deece868b5fa226eb9e7
Lukasz Majewski <l.majewski@samsung.com>
2014-12-08 18:04:20 +0100

    thermal: of: Extend of-thermal to export table of trip points
    This patch extends the of-thermal.c to export trip points for a given
    thermal zone.
    
    Thermal drivers should use of_thermal_get_trip_points() method to get
    pointer to table of thermal trip points.
    
    Signed-off-by: Lukasz Majewski <l.majewski@samsung.com>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>
    (cherry picked from commit ce8be7785922de0ef497b20384425ed04f674f9d)
    Signed-off-by: Kevin Hilman <khilman@linaro.org>
    Signed-off-by: Tkkg1994 <lg@linux.com>

--- drivers/thermal/thermal_core.h
+++ drivers/thermal/thermal_core.h
@@ -92,6 +92,8 @@ int of_parse_thermal_zones(void);
 void of_thermal_destroy_zones(void);
 int of_thermal_get_ntrips(struct thermal_zone_device *);
 bool of_thermal_is_trip_valid(struct thermal_zone_device *, int);
+const struct thermal_trip * const
+of_thermal_get_trip_points(struct thermal_zone_device *);
 #else
 static inline int of_parse_thermal_zones(void) { return 0; }
 static inline void of_thermal_destroy_zones(void) { }
@@ -103,6 +105,11 @@ static inline bool of_thermal_is_trip_valid(struct thermal_zone_device *tz,
 					    int trip)
 {
 	return 0;
+}
+static inline const struct thermal_trip * const
+of_thermal_get_trip_points(struct thermal_zone_device *tz)
+{
+	return NULL;
 }
 #endif
 
