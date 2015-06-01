77d9e9bf80d34c30c1b91d43914b31d3c4513429
halaszk <halaszk@deathlord.eu>
2015-06-01 13:21:29 +0200

    Drivers: sensorhub: reduce wakelock time
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- drivers/sensorhub/brcm/ssp_input.c
+++ drivers/sensorhub/brcm/ssp_input.c
@@ -440,7 +440,7 @@ void report_prox_data(struct ssp_data *data, struct sensor_value *proxdata)
 		ts_low);
 	input_sync(data->prox_input_dev);
 
-	wake_lock_timeout(&data->ssp_wake_lock, 2 * HZ);
+	wake_lock_timeout(&data->ssp_wake_lock, 1 * HZ);
 }
 
 void report_prox_raw_data(struct ssp_data *data,
@@ -537,7 +537,7 @@ void report_temp_humidity_data(struct ssp_data *data,
 		data->buf[TEMPERATURE_HUMIDITY_SENSOR].y);
 	input_sync(data->temp_humi_input_dev);
 	if (data->buf[TEMPERATURE_HUMIDITY_SENSOR].z)
-		wake_lock_timeout(&data->ssp_wake_lock, 2 * HZ);
+		wake_lock_timeout(&data->ssp_wake_lock, 1 * HZ);
 }
 
 #ifdef CONFIG_SENSORS_SSP_SHTC1
