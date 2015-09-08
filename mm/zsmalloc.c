e5369369d07c295230026e280e00a700785cf107
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2015-09-08 15:04:24 -0700

    zsmalloc: drop unused variable `nr_to_migrate'
    This patchset tweaks compaction and makes it possible to trigger pool
    compaction automatically when system is getting low on memory.
    
    zsmalloc in some cases can suffer from a notable fragmentation and
    compaction can release some considerable amount of memory.  The problem
    here is that currently we fully rely on user space to perform compaction
    when needed.  However, performing zsmalloc compaction is not always an
    obvious thing to do.  For example, suppose we have a `idle' fragmented
    (compaction was never performed) zram device and system is getting low
    on memory due to some 3rd party user processes (gcc LTO, or firefox,
    etc.).  It's quite unlikely that user space will issue zpool compaction
    in this case.  Besides, user space cannot tell for sure how badly pool
    is fragmented; however, this info is known to zsmalloc and, hence, to a
    shrinker.
    
    This patch (of 7):
    
    __zs_compact() does not use `nr_to_migrate', drop it.
    
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -2149,7 +2149,6 @@ static struct page *isolate_source_page(struct size_class *class)
 static unsigned long __zs_compact(struct zs_pool *pool,
 				struct size_class *class)
 {
-	int nr_to_migrate;
 	struct zs_compact_control cc;
 	struct page *src_page;
 	struct page *dst_page = NULL;
@@ -2160,8 +2159,6 @@ static unsigned long __zs_compact(struct zs_pool *pool,
 
 		BUG_ON(!is_first_page(src_page));
 
-		/* The goal is to migrate all live objects in source page */
-		nr_to_migrate = src_page->inuse;
 		cc.index = 0;
 		cc.s_page = src_page;
 
@@ -2176,7 +2173,6 @@ static unsigned long __zs_compact(struct zs_pool *pool,
 
 			putback_zspage(pool, class, dst_page);
 			nr_total_migrated += cc.nr_migrated;
-			nr_to_migrate -= cc.nr_migrated;
 		}
 
 		/* Stop if we couldn't find slot */
