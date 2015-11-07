506c18f8b1614d1bcacbfdd4f0b1cd66887b4eb1
Sergey SENOZHATSKY <sergey.senozhatsky@gmail.com>
2015-11-06 16:29:21 -0800

    mm: zsmalloc: constify struct zs_pool name
    Constify `struct zs_pool' ->name.
    
    [akpm@inux-foundation.org: constify zpool_create_pool()'s `type' arg also]
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Dan Streetman <ddstreet@ieee.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- include/linux/zsmalloc.h
+++ include/linux/zsmalloc.h
@@ -45,7 +45,7 @@ struct zs_ops {
 	int (*evict)(struct zs_pool *pool, unsigned long handle);
 };
 
-struct zs_pool *zs_create_pool(char *name, gfp_t flags, struct zs_ops *ops);
+struct zs_pool *zs_create_pool(const char *name, gfp_t flags);
 void zs_destroy_pool(struct zs_pool *pool);
 
 unsigned long zs_malloc(struct zs_pool *pool, size_t size);
