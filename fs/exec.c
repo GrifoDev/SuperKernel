490d6a5a816289821f12ae074e08754080ebbd37
Tkkg1994 <luca.grifo@outlook.com>
2016-03-28 19:54:26 +0200

    uskm: changes for UKSM and disabled by default

--- fs/exec.c
+++ fs/exec.c
@@ -56,6 +56,7 @@
 #include <linux/pipe_fs_i.h>
 #include <linux/oom.h>
 #include <linux/compat.h>
+#include <linux/ksm.h>
 
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
@@ -1195,6 +1196,7 @@ void setup_new_exec(struct linux_binprm * bprm)
 	/* An exec changes our domain. We are no longer part of the thread
 	   group */
 	current->self_exec_id++;
+
 	flush_signal_handlers(current, 0);
 }
 EXPORT_SYMBOL(setup_new_exec);
