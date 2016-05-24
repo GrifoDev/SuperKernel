d51c7f80001a5a239f7311a0632c24e51e4da737
Alex Shi <alex.shi@linaro.org>
2016-05-24 12:17:46 +0800

    Merge branch 'linux-linaro-lsk-v3.18' into linux-linaro-lsk-v3.18-android
    Signed-off-by: Tkkg1994 <lg@linux.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -5372,6 +5372,7 @@ static int usb_reset_and_verify_device(struct usb_device *udev)
 	}
 
 	bos = udev->bos;
+	udev->bos = NULL;
 
 	for (i = 0; i < SET_CONFIG_TRIES; ++i) {
 
@@ -5464,11 +5465,8 @@ done:
 	usb_set_usb2_hardware_lpm(udev, 1);
 	usb_unlocked_enable_lpm(udev);
 	usb_enable_ltm(udev);
-	/* release the new BOS descriptor allocated  by hub_port_init() */
-	if (udev->bos != bos) {
-		usb_release_bos_descriptor(udev);
-		udev->bos = bos;
-	}
+	usb_release_bos_descriptor(udev);
+	udev->bos = bos;
 	return 0;
 
 re_enumerate:
