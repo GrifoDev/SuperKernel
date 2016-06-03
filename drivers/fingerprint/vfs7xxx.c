ac3a08eda9b2b0f0a7d96052cee64fff89adeeef
Tkkg1994 <luca.grifo@outlook.com>
2016-06-03 14:50:21 +0200

    fingerprint: fixes: warning: ISO C90 forbids mixed declarations and code [-Wdeclaration-after-statement] u8 retry_cnt = 0;
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- drivers/fingerprint/vfs7xxx.c
+++ drivers/fingerprint/vfs7xxx.c
@@ -903,8 +903,8 @@ static long vfsspi_ioctl(struct file *filp, unsigned int cmd,
 			return -EFAULT;
 		}
 		if (onoff) {
-			(void)set_hmp_boost(1);
 			u8 retry_cnt = 0;
+			(void)set_hmp_boost(1);
 			pr_info("%s VFSSPI_IOCTL_CPU_SPEEDUP ON:%d, retry: %d\n",
 				__func__, onoff, retry_cnt);
 #if defined(CONFIG_SECURE_OS_BOOSTER_API)
