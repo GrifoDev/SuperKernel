f4406494f7b5e10582ea9eafbd17991a34d9f76f
Jesse Chan <jc@linux.com>
2016-06-03 17:06:57 +0800

    fingerprint: hmp_boost when fingerprint event happen
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- drivers/fingerprint/vfs7xxx.c
+++ drivers/fingerprint/vfs7xxx.c
@@ -903,6 +903,7 @@ static long vfsspi_ioctl(struct file *filp, unsigned int cmd,
 			return -EFAULT;
 		}
 		if (onoff) {
+			(void)set_hmp_boost(1);
 			u8 retry_cnt = 0;
 			pr_info("%s VFSSPI_IOCTL_CPU_SPEEDUP ON:%d, retry: %d\n",
 				__func__, onoff, retry_cnt);
@@ -919,6 +920,7 @@ static long vfsspi_ioctl(struct file *filp, unsigned int cmd,
 			} while (ret_val && retry_cnt < 7);
 #endif
 		} else {
+			(void)set_hmp_boost(0);
 			pr_info("%s VFSSPI_IOCTL_CPU_SPEEDUP OFF\n", __func__);
 #if defined(CONFIG_SECURE_OS_BOOSTER_API)
 			ret_val = secos_booster_stop();
