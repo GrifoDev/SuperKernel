bae89a8bbe4fd80859f975c71bf6c7fb26fc9096
Jesse Chan <jc@linux.com>
2016-06-03 17:06:57 +0800

    fingerprint: hmp_boost when fingerprint event happen
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>

--- drivers/fingerprint/et320-spi.c
+++ drivers/fingerprint/et320-spi.c
@@ -636,6 +636,7 @@ static long etspi_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	case FP_CPU_SPEEDUP:
 		pr_info("%s FP_CPU_SPEEDUP\n", __func__);
 		if (ioc->len) {
+			(void)set_hmp_boost(1);
 			u8 retry_cnt = 0;
 			pr_info("%s FP_CPU_SPEEDUP ON:%d, retry: %d\n",
 				__func__, ioc->len, retry_cnt);
@@ -652,6 +653,7 @@ static long etspi_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 			} while (retval && retry_cnt < 7);
 #endif
 		} else {
+			(void)set_hmp_boost(0);
 			pr_info("%s FP_CPU_SPEEDUP OFF\n", __func__);
 #if defined(CONFIG_SECURE_OS_BOOSTER_API)
 			retval = secos_booster_stop();
