273fa28c87431477ab39925a84af6021ef77a7f7
Tkkg1994 <luca.grifo@outlook.com>
2016-06-21 19:45:40 +0200

    sec_battery.c: STORE_MODE: create sysfs to control MIN & MAX battery capacity charge limits

--- drivers/battery/sec_battery.c
+++ drivers/battery/sec_battery.c
@@ -15,8 +15,13 @@
 #endif /* CONFIG_CCIC_NOTIFIER */
 #include <linux/moduleparam.h>
 
+static unsigned int STORE_MODE_CHARGING_MAX = 100;
+static unsigned int STORE_MODE_CHARGING_MIN = 95;
+
 static int wl_polling = 10;
 module_param(wl_polling, int, 0644);
+module_param_named(store_mode_max, STORE_MODE_CHARGING_MAX, uint, S_IWUSR | S_IRUGO);
+module_param_named(store_mode_min, STORE_MODE_CHARGING_MIN, uint, S_IWUSR | S_IRUGO);
 
 enum {
 	P9220_VOUT_0V = 0,
