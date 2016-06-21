32091db822c76ed3ab865b9103b82a3885da8463
Tkkg1994 <luca.grifo@outlook.com>
2016-06-21 22:27:17 +0200

    ramdisk: add all synapse files for hero2lte as well

---
+++ ramdisk/SM-G935F/ramdisk/res/crontab/cron-scripts/database_optimizing.sh
@@ -0,0 +1,42 @@
+#!/system/bin/sh
+
+# Optimize Databases script
+# Original by dorimanx for ExTweaks
+# Modified by UpInTheAir for SkyHigh kernels & Synapse
+
+BB=/system/xbin/busybox;
+SQLITE=$(cat /res/synapse/Super/cron_sqlite);
+
+if [ "$($BB mount | grep rootfs | cut -c 26-27 | grep -c ro)" -eq "1" ]; then
+	$BB mount -o remount,rw /;
+fi;
+
+if [ "$SQLITE" == 1 ]; then
+
+	# wait till CPU is idle.
+	while [ ! "$($BB cat /proc/loadavg | cut -c1-4)" -lt "3.50" ]; do
+		echo "Waiting For CPU to cool down";
+		sleep 30;
+	done;
+
+	for i in $(find /data -iname "*.db"); do
+		/system/xbin/sqlite3 "$i" 'VACUUM;' 2> /dev/null;
+		/system/xbin/sqlite3 "$i" 'REINDEX;' 2> /dev/null;
+	done;
+
+	for i in $(find /sdcard -iname "*.db"); do
+		/system/xbin/sqlite3 "$i" 'VACUUM;' 2> /dev/null;
+		/system/xbin/sqlite3 "$i" 'REINDEX;' 2> /dev/null;
+	done;
+	sync;
+
+	date +%R-%F > /data/crontab/cron-db-optimizing;
+	echo " DB Optimized" >> /data/crontab/cron-db-optimizing;
+
+elif [ "$SQLITE" == 0 ]; then
+
+	date +%R-%F > /data/crontab/cron-db-optimizing;
+	echo " DB Optimization is disabled" >> /data/crontab/cron-db-optimizing;
+fi;
+
+$BB mount -t rootfs -o remount,ro rootfs;
---
+++ ramdisk/SM-G935F/ramdisk/res/crontab/cron-scripts/ram_release.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+
+# Original by dorimanx for ExTweaks
+# Modified by UpInTheAir for SkyHigh kernels & Synapse
+
+BB=/system/xbin/busybox;
+GOOGLE=$(cat /res/synapse/Super/cron_google);
+
+if [ "$($BB mount | grep rootfs | cut -c 26-27 | grep -c ro)" -eq "1" ]; then
+	$BB mount -o remount,rw /;
+fi;
+
+if [ "$GOOGLE" == 1 ]; then
+
+	if [ "$($BB pidof com.google.android.gms | wc -l)" -eq "1" ]; then
+		$BB kill $($BB pidof com.google.android.gms);
+	fi;
+	if [ "$($BB pidof com.google.android.gms.unstable | wc -l)" -eq "1" ]; then
+		$BB kill $($BB pidof com.google.android.gms.unstable);
+	fi;
+	if [ "$($BB pidof com.google.android.gms.persistent | wc -l)" -eq "1" ]; then
+		$BB kill $($BB pidof com.google.android.gms.persistent);
+	fi;
+	if [ "$($BB pidof com.google.android.gms.wearable | wc -l)" -eq "1" ]; then
+		$BB kill $($BB pidof com.google.android.gms.wearable);
+	fi;
+
+	date +%R-%F > /data/crontab/cron-ram-release;
+	echo " Google RAM released" >> /data/crontab/cron-ram-release;
+
+elif [ "$GOOGLE" == 0 ]; then
+
+	date +%R-%F > /data/crontab/cron-ram-release;
+	echo " Google RAM Release is disabled" >> /data/crontab/cron-ram-release;
+fi;
+
+$BB mount -t rootfs -o remount,ro rootfs;
