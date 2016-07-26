1eafe7b5a582bef6fc3b5017db924174d8379080
Tkkg1994 <lg@linux.com>
2016-07-26 16:34:06 +0200

    disable ptrace error warnings, we can't read anything in kernel log!

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -244,10 +244,11 @@ static int __ptrace_may_access(struct task_struct *task, unsigned int mode)
 	kuid_t caller_uid;
 	kgid_t caller_gid;
 
-	if (!(mode & PTRACE_MODE_FSCREDS) == !(mode & PTRACE_MODE_REALCREDS)) {
-		WARN(1, "denying ptrace access check without PTRACE_MODE_*CREDS\n");
-		return -EPERM;
-	}
+/*	if (!(mode & PTRACE_MODE_FSCREDS) == !(mode & PTRACE_MODE_REALCREDS)) {
+ *		WARN(1, "denying ptrace access check without PTRACE_MODE_*CREDS\n");
+ *		return -EPERM;
+ *	}
+ */
 
 	/* May we inspect the given task?
 	 * This check is used both for attaching with ptrace
