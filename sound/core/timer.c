2ce101a04436cfc1df0a854b3ce54198f96f92be
Alex Shi <alex.shi@linaro.org>
2016-08-01 21:29:07 +0800

    Merge branch 'linux-linaro-lsk-v3.18' into linux-linaro-lsk-v3.18-android
    Signed-off-by: Tkkg1994 <lg@linux.com>

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -1967,6 +1967,7 @@ static ssize_t snd_timer_user_read(struct file *file, char __user *buffer,
 
 		qhead = tu->qhead++;
 		tu->qhead %= tu->queue_size;
+		tu->qused--;
 		spin_unlock_irq(&tu->qlock);
 
 		if (tu->tread) {
@@ -1980,7 +1981,6 @@ static ssize_t snd_timer_user_read(struct file *file, char __user *buffer,
 		}
 
 		spin_lock_irq(&tu->qlock);
-		tu->qused--;
 		if (err < 0)
 			goto _error;
 		result += unit;
