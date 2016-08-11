99ea36778cb123cca9d2caeee13dbeb8618677da
Tkkg1994 <lg@linux.com>
2016-08-11 17:19:29 +0200

    Revert "selinux: make it permissive my way" Maybe make private mode not fully working, needs testing with different selinux patch This reverts commit c6d784da6362a3a9f845db7c0c26da92f8680f9b.

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -187,8 +187,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	selnl_notify_setenforce(new_value);
 	selinux_status_update_setenforce(new_value);
 #else
-	// If build is user build and permissive option is set, selinux is always permissive
-	new_value = 0;
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
