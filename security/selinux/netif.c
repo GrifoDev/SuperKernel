16937bb794d9a9eb680ecdf7e1b38285fe424b6a
Tkkg1994 <lg@linux.com>
2016-09-19 20:19:04 +0200

    selinux: remove whitespace

--- security/selinux/netif.c
+++ security/selinux/netif.c
@@ -276,7 +276,7 @@ static struct notifier_block sel_netif_netdev_notifier = {
 static __init int sel_netif_init(void)
 {
 	int i;
-	
+
 #ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	selinux_enabled = 1;
 #endif
