e59f1bf008bbfc39844c67ce4877f1e5fd79f4db
lyapota <lyapic@ukr.net>
2017-02-17 22:38:01 +0200

    set android version

--- scripts/android-version.sh
+++ scripts/android-version.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-MAJOR=$(echo 7 | cut -d '.' -f 1)
+$1="7.0.1"
+
+MAJOR=$(echo $1 | cut -d '.' -f 1)
 MINOR=$(echo $1 | cut -d '.' -f 2)
 PATCH=$(echo $1 | cut -d '.' -f 3)
 if [ "x$PATCH" != "x" ] ; then
