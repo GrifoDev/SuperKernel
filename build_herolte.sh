110e89a2a2851079870b19e475ba905a4872ea0f
Luca Grifo <lg@linux.com>
2017-02-18 02:07:09 +0100

    build: release V2.2.0 Makefile: new 6.3.1 linaro toolchain

--- build_herolte.sh
+++ build_herolte.sh
@@ -4,8 +4,8 @@
 export MODEL=herolte
 export VARIANT=eur
 export ARCH=arm64
-export VERSION=V2.1.5
-export BUILD_CROSS_COMPILE=../Toolchain/aarch64-cortex_a53-linux-gnueabi-6.3.0/bin/aarch64-cortex_a53-linux-gnueabi-
+export VERSION=V2.2.0
+export BUILD_CROSS_COMPILE=../Toolchain/aarch64-cortex_a53-linux-gnueabi-linaro-6.3.1/bin/aarch64-cortex_a53-linux-gnueabi-
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 RDIR=$(pwd)
