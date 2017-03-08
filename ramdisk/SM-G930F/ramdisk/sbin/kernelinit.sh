18ea0736e40b4f86812c9d6ca0247e5d4fdeb312
Luca Grifo <lg@linux.com>
2017-03-08 20:55:33 +0100

    ramdisk: update script behavior and delete unused default.prop lines

---
+++ ramdisk/SM-G930F/ramdisk/sbin/kernelinit.sh
@@ -0,0 +1,30 @@
+#!/system/bin/sh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Knox set to 0 on working system
+/sbin/resetprop -n ro.boot.warranty_bit "0"
+/sbin/resetprop -n ro.warranty_bit "0"
+
+# Fix some safetynet flags
+/sbin/resetprop -n ro.boot.veritymode "enforcing"
+/sbin/resetprop -n ro.boot.verifiedbootstate "green"
+/sbin/resetprop -n ro.boot.flash.locked "1"
+/sbin/resetprop -n ro.boot.ddrinfo "00000001"
+
+# Deepsleep fix
+su -c 'echo "temporary none" >> /sys/class/scsi_disk/0:0:0:0/cache_type'
+su -c 'echo "temporary none" >> /sys/class/scsi_disk/0:0:0:1/cache_type'
+su -c 'echo "temporary none" >> /sys/class/scsi_disk/0:0:0:2/cache_type'
+su -c 'echo "temporary none" >> /sys/class/scsi_disk/0:0:0:3/cache_type'
