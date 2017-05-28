71f2368a13ba931efa447e3181155f438d855e13
Luca Grifo <lg@linux.com>
2017-05-28 02:08:58 +0200

    build: release V2.6

--- build_herolte.sh
+++ build_herolte.sh
@@ -4,7 +4,7 @@
 export MODEL=herolte
 export VARIANT=eur
 export ARCH=arm64
-export VERSION=V2.5.2
+export VERSION=V2.6.0
 export BUILD_CROSS_COMPILE=../Toolchain/aarch64-cortex_a53-linux-gnueabi-6.3.0/bin/aarch64-cortex_a53-linux-gnueabi-
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
