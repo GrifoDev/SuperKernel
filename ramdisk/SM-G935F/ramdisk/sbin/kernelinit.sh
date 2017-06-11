079bd306debe404c6475b18c5117d33223168268
Luca Grifo <lg@linux.com>
2017-06-11 18:29:08 +0200

    ramdisk: some more flags for samsung
    Signed-off-by: Luca Grifo <lg@linux.com>

--- ramdisk/SM-G935F/ramdisk/sbin/kernelinit.sh
+++ ramdisk/SM-G935F/ramdisk/sbin/kernelinit.sh
@@ -13,15 +13,20 @@
 # limitations under the License.
 #
 
-# Knox set to 0 on working system
+# Knox set to 0 on running system
 /sbin/resetprop -n ro.boot.warranty_bit "0"
 /sbin/resetprop -n ro.warranty_bit "0"
 
-# Fix some safetynet flags
+# Fix safetynet flags
 /sbin/resetprop -n ro.boot.veritymode "enforcing"
 /sbin/resetprop -n ro.boot.verifiedbootstate "green"
 /sbin/resetprop -n ro.boot.flash.locked "1"
 /sbin/resetprop -n ro.boot.ddrinfo "00000001"
+
+# Samsung related flags
+/sbin/resetprop -n ro.fmp_config "1"
+/sbin/resetprop -n ro.boot.fmp_config "1"
+/sbin/resetprop -n sys.oem_unlock_allowed "0"
 
 # Deepsleep fix
 su -c 'echo "temporary none" >> /sys/class/scsi_disk/0:0:0:0/cache_type'
