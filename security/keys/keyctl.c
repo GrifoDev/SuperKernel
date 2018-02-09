4947542092164d9c2dfabc2ae90150a5872fff5f
Tkkg1994 <luca@grifodev.ch>
2018-02-09 13:33:41 +0100

    source: G935FXXU2DRAG
    Signed-off-by: Tkkg1994 <luca@grifodev.ch>

--- security/keys/keyctl.c
+++ security/keys/keyctl.c
@@ -743,6 +743,10 @@ long keyctl_read_key(key_serial_t keyid, char __user *buffer, size_t buflen)
 	}
 
 	key = key_ref_to_ptr(key_ref);
+	if (test_bit(KEY_FLAG_NEGATIVE, &key->flags)) {
+		ret = -ENOKEY;
+		goto error2;
+	}
 
 	if (test_bit(KEY_FLAG_NEGATIVE, &key->flags)) {
 		ret = -ENOKEY;
